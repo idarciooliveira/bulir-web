99d10649e2076dd75da6a9dc64eff57f49665f56
idarciooliveira <idarciooliveira@gmail.com>
2024-11-16 02:19:21 +0100

    added role control

--- app/dashboard/bookings/page.tsx
+++ app/dashboard/bookings/page.tsx
@@ -2,22 +2,33 @@ import BookingList, { Booking } from "@/components/custom/booking-list";
 import { authConfig } from "@/lib/auth.config";
 import { api } from "@/services/api";
 import { getServerSession } from "next-auth";
+import { unstable_noStore } from "next/cache";
 import React from "react";
 
 const getBookings = async () => {
   const session = await getServerSession(authConfig);
-  const response = await api.get<Booking[]>(
-    `/bookings/${session?.user.id}/custumers`
-  );
-  return response.data;
+  if (session?.user.role == "CUSTUMER") {
+    const response = await api.get<Booking[]>(
+      `/bookings/${session?.user.id}/custumers`
+    );
+    return response.data;
+  } else {
+    const response = await api.get<Booking[]>(
+      `/bookings/${session?.user.id}/providers`
+    );
+    return response.data;
+  }
 };
+
+unstable_noStore();
 
 export default async function BookingsPage() {
   const bookings = await getBookings();
+  const session = await getServerSession(authConfig);
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Bookings</h1>
-      <BookingList Bookings={bookings} />
+      <BookingList userRole={session?.user.role} Bookings={bookings} />
     </div>
   );
 }
--- components/custom/booking-list.tsx
+++ components/custom/booking-list.tsx
@@ -1,4 +1,8 @@
+"use client";
 import React from "react";
+import { Button } from "../ui/button";
+import { api } from "@/services/api";
+import { useRouter } from "next/navigation";
 
 export type Booking = {
   id: string;
@@ -18,8 +22,33 @@ export type Booking = {
 
 type BookingListProps = {
   Bookings: Booking[];
+  userRole: string;
 };
-export default function BookingList({ Bookings }: BookingListProps) {
+
+export enum BookingStatus {
+  COMPLETED = "COMPLETA",
+  CANCELLED = "CANCELADO",
+  CONFIRMED = "CONFIRMADO",
+  PENDING = "PENDENTE",
+}
+export default function BookingList({ Bookings, userRole }: BookingListProps) {
+  const router = useRouter();
+
+  const approveBooking = async (bookingId: string) => {
+    await api.patch(`/bookings/${bookingId}/confirm`);
+    router.refresh();
+  };
+
+  const cancelBooking = async (bookingId: string) => {
+    await api.patch(`/bookings/${bookingId}/cancel`);
+    router.refresh();
+  };
+
+  const completeBooking = async (bookingId: string) => {
+    await api.patch(`/bookings/${bookingId}/complete`);
+    router.refresh();
+  };
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {Bookings.map((booking, index) => (
@@ -32,6 +61,38 @@ export default function BookingList({ Bookings }: BookingListProps) {
           <p>Start Date: {new Date(booking.startAt).toLocaleDateString()}</p>
           <p>Provider: {booking.provider.fullname}</p>
           <p>Price: ${booking.total.toFixed(2)}</p>
+          {userRole !== "CUSTUMER" && (
+            <div className="w-full flex flex-row space-x-2 mt-4">
+              {booking.status == BookingStatus.PENDING && (
+                <Button
+                  onClick={() => approveBooking(booking.id)}
+                  type="button"
+                  className="bg-green-500"
+                >
+                  Accept
+                </Button>
+              )}
+              {booking.status == BookingStatus.CONFIRMED && (
+                <Button
+                  onClick={() => completeBooking(booking.id)}
+                  type="button"
+                  className="bg-orange-500"
+                >
+                  COMPLETE
+                </Button>
+              )}
+              {booking.status !== BookingStatus.CANCELLED &&
+                booking.status !== BookingStatus.COMPLETED && (
+                  <Button
+                    onClick={() => cancelBooking(booking.id)}
+                    type="button"
+                    variant={"destructive"}
+                  >
+                    Reject
+                  </Button>
+                )}
+            </div>
+          )}
         </div>
       ))}
     </div>
